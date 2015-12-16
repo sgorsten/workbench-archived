7df418e57c30c5fcd0b61d71e8511a3ca3659c66
Sterling Orsten <sgorsten@gmail.com>
2015-12-15 21:49:09 -0800

    Some modifications to layout.

--- graph-editor/graph-editor.cpp
+++ graph-editor/graph-editor.cpp
@@ -16,49 +16,9 @@ void draw_tooltip(draw_buffer_2d & buffer, const int2 & loc, utf8::string_view t
 
 struct node_type
 {
-    static const int corner_radius = 10;
-    static const int title_height = 25;
-
     std::string caption;
     std::vector<std::string> inputs;
     std::vector<std::string> outputs;
-
-    int2 get_input_location(const rect & r, size_t index) const { return {r.x0, r.y0 + title_height + 18 + 24 * (int)index}; }
-    int2 get_output_location(const rect & r, size_t index) const { return {r.x1, r.y0 + title_height + 18 + 24 * (int)index}; }
-
-    void draw(gui & g, const rect & r) const
-    {
-        g.draw_partial_rounded_rect({r.x0, r.y0, r.x1, r.y0+title_height}, corner_radius, {0.5f,0.5f,0.5f,0.85f}, true, true, false, false);
-        g.draw_partial_rounded_rect({r.x0, r.y0+title_height, r.x1, r.y1}, corner_radius, {0.3f,0.3f,0.3f,0.85f}, false, false, true, true);
-        g.begin_scissor({r.x0, r.y0, r.x1, r.y0+title_height});
-        g.draw_shadowed_text({r.x0+8, r.y0+6}, caption, {1,1,1,1});
-        g.end_scissor();
-
-        for(size_t i=0; i<inputs.size(); ++i)
-        {
-            const auto loc = get_input_location(r,i);
-            g.draw_circle(loc, 8, {1,1,1,1});
-            g.draw_circle(loc, 6, {0.2f,0.2f,0.2f,1});
-            g.draw_shadowed_text(loc + int2(12, -g.buffer.get_library().default_font.line_height/2), inputs[i], {1,1,1,1});
-
-            if(g.is_cursor_over({loc.x-8, loc.y-8, loc.x+8, loc.y+8}))
-            {
-                draw_tooltip(g.buffer, loc, "This is an input");
-            }
-        }
-        for(size_t i=0; i<outputs.size(); ++i)
-        {
-            const auto loc = get_output_location(r,i);
-            g.draw_circle(loc, 8, {1,1,1,1});
-            g.draw_circle(loc, 6, {0.2f,0.2f,0.2f,1});
-            g.draw_shadowed_text(loc + int2(-12 - g.buffer.get_library().default_font.get_text_width(outputs[i]), -g.buffer.get_library().default_font.line_height/2), outputs[i], {1,1,1,1});
-
-            if(g.is_cursor_over({loc.x-8, loc.y-8, loc.x+8, loc.y+8}))
-            {
-                draw_tooltip(g.buffer, loc, "This is an output");
-            }
-        }
-    }
 };
 
 struct editor_state;
@@ -77,17 +37,31 @@ struct edge
 struct node
 {
     const node_type * type;
-    rect placement;
+    int2 placement;
 
     std::vector<edge> input_edges;
 
-    node(const node_type * type, const rect & placement) : type(type), placement(placement), input_edges(type->inputs.size()) {}
+    node(const node_type * type, const int2 & placement) : type(type), placement(placement), input_edges(type->inputs.size()) {}
 };
 
-static int2 get_input_location(const node & n, size_t index) { return n.type->get_input_location(n.placement, index); }
-static int2 get_output_location(const node & n, size_t index) { return n.type->get_output_location(n.placement, index); }
-static rect get_input_rect(const node & n, size_t index) { auto loc = get_input_location(n,index); return {loc.x-8, loc.y-8, loc.x+8, loc.y+8}; }
-static rect get_output_rect(const node & n, size_t index) { auto loc = get_output_location(n,index); return {loc.x-8, loc.y-8, loc.x+8, loc.y+8}; }
+const int corner_radius = 10;
+const int title_height = 25;
+static int get_node_width(const gui & g, const node & n) 
+{ 
+    int l=0, r=0;
+    for(auto & in : n.type->inputs) l = std::max(l, g.sprites.default_font.get_text_width(in));
+    for(auto & out : n.type->outputs) r = std::max(r, g.sprites.default_font.get_text_width(out));
+    return std::max(l + r + 50, g.sprites.default_font.get_text_width(n.type->caption) + 16);
+}
+static int get_in_pins(const node & n) { return static_cast<int>(n.type->inputs.size()); }
+static int get_out_pins(const node & n) { return static_cast<int>(n.type->outputs.size()); }
+static int get_node_body_height(const node & n) { return std::max(get_in_pins(n), get_out_pins(n)) * 24 + 12; }
+static int get_node_height(const node & n) { return title_height + get_node_body_height(n); }
+static rect get_node_rect(const gui & g, const node & n) { return {n.placement.x, n.placement.y, n.placement.x+get_node_width(g,n), n.placement.y+get_node_height(n)}; }
+static int2 get_input_location (const gui & g, const node & n, size_t index) { auto r = get_node_rect(g,n); return {r.x0, r.y0 + title_height + 18 + 24 * (int)index + std::max(get_out_pins(n) - get_in_pins(n), 0) * 12}; }
+static int2 get_output_location(const gui & g, const node & n, size_t index) { auto r = get_node_rect(g,n); return {r.x1, r.y0 + title_height + 18 + 24 * (int)index + std::max(get_in_pins(n) - get_out_pins(n), 0) * 12}; }
+static rect get_input_rect (const gui & g, const node & n, size_t index) { auto loc = get_input_location(g,n,index); return {loc.x-8, loc.y-8, loc.x+8, loc.y+8}; }
+static rect get_output_rect(const gui & g, const node & n, size_t index) { auto loc = get_output_location(g,n,index); return {loc.x-8, loc.y-8, loc.x+8, loc.y+8}; }
 
 const node_type types[] = {
     {"Add", {"A", "B"}, {"A+B"}},
@@ -129,7 +103,7 @@ struct graph
             for(size_t i=0; i<n->input_edges.size(); ++i)
             {
                 if(!n->input_edges[i].other) continue;
-                const auto p0 = float2(get_output_location(*n->input_edges[i].other, n->input_edges[i].pin)), p3 = float2(get_input_location(*n,i));
+                const auto p0 = float2(get_output_location(g, *n->input_edges[i].other, n->input_edges[i].pin)), p3 = float2(get_input_location(g, *n, i));
                 const auto p1 = float2(p0.x + abs(p3.x - p0.x)*0.7f, p0.y), p2 = float2(p3.x - abs(p3.x - p0.x)*0.7f, p3.y);
                 g.draw_bezier_curve(p0, p1, p2, p3, 2, {1,1,1,1});
             }
@@ -141,7 +115,7 @@ struct graph
             // Input pin interactions
             for(size_t i=0; i<n->type->inputs.size(); ++i)
             {
-                if(g.check_click(ID_NEW_WIRE, get_input_rect(*n,i)))
+                if(g.check_click(ID_NEW_WIRE, get_input_rect(g,*n,i)))
                 {
                     n->input_edges[i].other = nullptr;
                     link_input_node = n;
@@ -149,7 +123,7 @@ struct graph
                     g.consume_input();
                 }
 
-                if(g.is_cursor_over(get_input_rect(*n,i)) && link_output_node && g.check_release(ID_NEW_WIRE))
+                if(g.is_cursor_over(get_input_rect(g,*n,i)) && link_output_node && g.check_release(ID_NEW_WIRE))
                 {
                     n->input_edges[i] = {link_output_node, link_output_pin};
                     reset_link();
@@ -159,7 +133,7 @@ struct graph
             // Output pin interactions
             for(size_t i=0; i<n->type->outputs.size(); ++i)
             {
-                if(g.check_click(ID_NEW_WIRE, get_output_rect(*n,i)))
+                if(g.check_click(ID_NEW_WIRE, get_output_rect(g,*n,i)))
                 {
                     if(g.is_alt_held()) for(auto & other : nodes) for(auto & edge : other->input_edges) if(edge.other == n) edge.other = nullptr;
                     link_output_node = n;
@@ -167,7 +141,7 @@ struct graph
                     g.consume_input();
                 }
 
-                if(g.is_cursor_over(get_output_rect(*n,i)) && link_input_node && g.check_release(ID_NEW_WIRE))
+                if(g.is_cursor_over(get_output_rect(g,*n,i)) && link_input_node && g.check_release(ID_NEW_WIRE))
                 {
                     link_input_node->input_edges[link_input_pin] = {n, i};
                     reset_link();
@@ -175,18 +149,44 @@ struct graph
             }
 
             // Drag the body of the node
-            if(g.check_click(id, n->placement)) g.consume_input();
+            if(g.check_click(id, get_node_rect(g,*n))) g.consume_input();
             if(g.check_pressed(id))
             {
-                int2 delta = int2(g.get_cursor() - g.click_offset) - int2(n->placement.x0, n->placement.y0);
-                n->placement.x0 += delta.x;
-                n->placement.y0 += delta.y;
-                n->placement.x1 += delta.x;
-                n->placement.y1 += delta.y;
+                n->placement = int2(g.get_cursor() - g.click_offset);
             }
 
             // Draw the node
-            n->type->draw(g, n->placement);
+            auto r = get_node_rect(g,*n);
+            g.draw_partial_rounded_rect({r.x0, r.y0, r.x1, r.y0+title_height}, corner_radius, {0.5f,0.5f,0.5f,0.85f}, true, true, false, false);
+            g.draw_partial_rounded_rect({r.x0, r.y0+title_height, r.x1, r.y1}, corner_radius, {0.3f,0.3f,0.3f,0.85f}, false, false, true, true);
+            g.begin_scissor({r.x0, r.y0, r.x1, r.y0+title_height});
+            g.draw_shadowed_text({r.x0+8, r.y0+6}, n->type->caption, {1,1,1,1});
+            g.end_scissor();
+
+            for(size_t i=0; i<n->type->inputs.size(); ++i)
+            {
+                const auto loc = get_input_location(g,*n,i);
+                g.draw_circle(loc, 8, {1,1,1,1});
+                g.draw_circle(loc, 6, {0.2f,0.2f,0.2f,1});
+                g.draw_shadowed_text(loc + int2(12, -g.buffer.get_library().default_font.line_height/2), n->type->inputs[i], {1,1,1,1});
+
+                if(g.is_cursor_over({loc.x-8, loc.y-8, loc.x+8, loc.y+8}))
+                {
+                    draw_tooltip(g.buffer, loc, "This is an input");
+                }
+            }
+            for(size_t i=0; i<n->type->outputs.size(); ++i)
+            {
+                const auto loc = get_output_location(g,*n,i);
+                g.draw_circle(loc, 8, {1,1,1,1});
+                g.draw_circle(loc, 6, {0.2f,0.2f,0.2f,1});
+                g.draw_shadowed_text(loc + int2(-12 - g.buffer.get_library().default_font.get_text_width(n->type->outputs[i]), -g.buffer.get_library().default_font.line_height/2), n->type->outputs[i], {1,1,1,1});
+
+                if(g.is_cursor_over({loc.x-8, loc.y-8, loc.x+8, loc.y+8}))
+                {
+                    draw_tooltip(g.buffer, loc, "This is an output");
+                }
+            }
             ++id;
         }
 
@@ -196,8 +196,8 @@ struct graph
             for(size_t i=0; i<n->input_edges.size(); ++i)
             {
                 if(!n->input_edges[i].other) continue;
-                g.draw_circle(get_output_location(*n->input_edges[i].other, n->input_edges[i].pin), 7, {1,1,1,1});
-                g.draw_circle(get_input_location(*n, i), 7, {1,1,1,1});
+                g.draw_circle(get_output_location(g, *n->input_edges[i].other, n->input_edges[i].pin), 7, {1,1,1,1});
+                g.draw_circle(get_input_location(g, *n, i), 7, {1,1,1,1});
             }
         }
 
@@ -207,13 +207,13 @@ struct graph
             auto p0 = g.get_cursor(), p3 = p0;
             if(link_output_node)
             {
-                auto loc = get_output_location(*link_output_node, link_output_pin);
+                auto loc = get_output_location(g, *link_output_node, link_output_pin);
                 g.draw_circle(loc, 7, {1,1,1,1});
                 p0 = float2(loc);
             }
             if(link_input_node)
             {
-                auto loc = get_input_location(*link_input_node, link_input_pin);
+                auto loc = get_input_location(g, *link_input_node, link_input_pin);
                 g.draw_circle(loc, 7, {1,1,1,1});
                 p3 = float2(loc);
             }
@@ -247,7 +247,7 @@ struct graph
                 rect r = {loc.x, loc.y, loc.x + 100, loc.y + g.sprites.default_font.line_height};
                 if(g.check_click(ID_POPUP_MENU, r))
                 {
-                    nodes.push_back(new node{&type, {loc.x, loc.y, loc.x+100, loc.y+100}});
+                    nodes.push_back(new node{&type, loc});
                     g.pressed_id = {};
                 }
                 g.draw_shadowed_text(loc, type.caption, {1,1,1,1});
@@ -260,7 +260,6 @@ struct graph
         scrollable_zoomable_background(g, ID_DRAG_GRAPH, view);
 
         g.end_transform();
-
     }
 };
 
@@ -282,8 +281,8 @@ int main()
     graph gr;
     gr.view = {1,{0,0}};
     gr.nodes = {
-        new node{&types[0], {50,50,300,250}},
-        new node{&types[1], {650,150,900,350}}
+        new node{&types[0], {50,50}},
+        new node{&types[1], {650,150}}
     };
     gr.nodes[1]->input_edges[1] = edge(gr.nodes[0], 0);
 
