8cec64588588883f87b5c40f282758c25c9cca9d
Sterling Orsten <sgorsten@gmail.com>
2015-12-15 19:16:12 -0800

    Support for adding new nodes.

--- graph-editor/graph-editor.cpp
+++ graph-editor/graph-editor.cpp
@@ -82,17 +82,27 @@ struct node
     std::vector<edge> input_edges;
 
     node(const node_type * type, const rect & placement) : type(type), placement(placement), input_edges(type->inputs.size()) {}
+};
 
-    int2 get_input_location(size_t index) const { return type->get_input_location(placement, index); }
-    int2 get_output_location(size_t index) const { return type->get_output_location(placement, index); }
-    rect get_input_rect(size_t index) const { auto loc = get_input_location(index); return {loc.x-8, loc.y-8, loc.x+8, loc.y+8}; }
-    rect get_output_rect(size_t index) const { auto loc = get_output_location(index); return {loc.x-8, loc.y-8, loc.x+8, loc.y+8}; }
+static int2 get_input_location(const node & n, size_t index) { return n.type->get_input_location(n.placement, index); }
+static int2 get_output_location(const node & n, size_t index) { return n.type->get_output_location(n.placement, index); }
+static rect get_input_rect(const node & n, size_t index) { auto loc = get_input_location(n,index); return {loc.x-8, loc.y-8, loc.x+8, loc.y+8}; }
+static rect get_output_rect(const node & n, size_t index) { auto loc = get_output_location(n,index); return {loc.x-8, loc.y-8, loc.x+8, loc.y+8}; }
+
+const node_type types[] = {
+    {"Add", {"A", "B"}, {"A+B"}},
+    {"Subtract", {"A", "B"}, {"A-B"}},
+    {"Multiply", {"A", "B"}, {"A*B"}},
+    {"Divide", {"A", "B"}, {"A/B"}},
+    {"Break Float3", {""}, {"X", "Y", "Z"}},
+    {"Make Float3", {"X", "Y", "Z"}, {""}},
+    {"Normalize Vector", {""}, {""}},
 };
 
 struct graph
 {
     transform_2d view;
-    std::vector<node> nodes;
+    std::vector<node *> nodes;
 
     node * link_input_node, * link_output_node;
     size_t link_input_pin, link_output_pin;
@@ -104,18 +114,22 @@ struct graph
         link_input_node = link_output_node = nullptr;
     }
 
+    bool mr = false;
+
     void on_gui(gui & g)
     {
-        int id = 2;
+        const int ID_NEW_WIRE = 1, ID_POPUP_MENU = 2, ID_DRAG_GRAPH = 3;
+
+        int id = 4;
         g.begin_transform(view);
 
         // Draw wires
         for(auto & n : nodes)
         {
-            for(size_t i=0; i<n.input_edges.size(); ++i)
+            for(size_t i=0; i<n->input_edges.size(); ++i)
             {
-                if(!n.input_edges[i].other) continue;
-                const auto p0 = float2(n.input_edges[i].other->get_output_location(n.input_edges[i].pin)), p3 = float2(n.get_input_location(i));
+                if(!n->input_edges[i].other) continue;
+                const auto p0 = float2(get_output_location(*n->input_edges[i].other, n->input_edges[i].pin)), p3 = float2(get_input_location(*n,i));
                 const auto p1 = float2(p0.x + abs(p3.x - p0.x)*0.7f, p0.y), p2 = float2(p3.x - abs(p3.x - p0.x)*0.7f, p3.y);
                 g.draw_bezier_curve(p0, p1, p2, p3, 2, {1,1,1,1});
             }
@@ -125,92 +139,125 @@ struct graph
         for(auto & n : nodes)
         {
             // Input pin interactions
-            for(size_t i=0; i<n.type->inputs.size(); ++i)
+            for(size_t i=0; i<n->type->inputs.size(); ++i)
             {
-                if(g.check_click(1, n.get_input_rect(i)))
+                if(g.check_click(ID_NEW_WIRE, get_input_rect(*n,i)))
                 {
-                    n.input_edges[i].other = nullptr;
-                    link_input_node = &n;
+                    n->input_edges[i].other = nullptr;
+                    link_input_node = n;
                     link_input_pin = i;
                     g.consume_input();
                 }
 
-                if(g.is_cursor_over(n.get_input_rect(i)) && link_output_node && g.check_release(1))
+                if(g.is_cursor_over(get_input_rect(*n,i)) && link_output_node && g.check_release(ID_NEW_WIRE))
                 {
-                    n.input_edges[i] = {link_output_node, link_output_pin};
+                    n->input_edges[i] = {link_output_node, link_output_pin};
                     reset_link();
                 }
             }
             
             // Output pin interactions
-            for(size_t i=0; i<n.type->outputs.size(); ++i)
+            for(size_t i=0; i<n->type->outputs.size(); ++i)
             {
-                if(g.check_click(1, n.get_output_rect(i)))
+                if(g.check_click(ID_NEW_WIRE, get_output_rect(*n,i)))
                 {
-                    if(g.is_alt_held()) for(auto & other : nodes) for(auto & edge : other.input_edges) if(edge.other == &n) edge.other = nullptr;
-                    link_output_node = &n;
+                    if(g.is_alt_held()) for(auto & other : nodes) for(auto & edge : other->input_edges) if(edge.other == n) edge.other = nullptr;
+                    link_output_node = n;
                     link_output_pin = i;
                     g.consume_input();
                 }
 
-                if(g.is_cursor_over(n.get_output_rect(i)) && link_input_node && g.check_release(1))
+                if(g.is_cursor_over(get_output_rect(*n,i)) && link_input_node && g.check_release(ID_NEW_WIRE))
                 {
-                    link_input_node->input_edges[link_input_pin] = {&n, i};
+                    link_input_node->input_edges[link_input_pin] = {n, i};
                     reset_link();
                 }
             }
 
             // Drag the body of the node
-            if(g.check_click(id, n.placement)) g.consume_input();
+            if(g.check_click(id, n->placement)) g.consume_input();
             if(g.check_pressed(id))
             {
-                int2 delta = int2(g.get_cursor() - g.click_offset) - int2(n.placement.x0, n.placement.y0);
-                n.placement.x0 += delta.x;
-                n.placement.y0 += delta.y;
-                n.placement.x1 += delta.x;
-                n.placement.y1 += delta.y;
+                int2 delta = int2(g.get_cursor() - g.click_offset) - int2(n->placement.x0, n->placement.y0);
+                n->placement.x0 += delta.x;
+                n->placement.y0 += delta.y;
+                n->placement.x1 += delta.x;
+                n->placement.y1 += delta.y;
             }
 
             // Draw the node
-            n.type->draw(g, n.placement);
+            n->type->draw(g, n->placement);
             ++id;
         }
 
         // Fill in pins with connected wires
         for(auto & n : nodes)
         {
-            for(size_t i=0; i<n.input_edges.size(); ++i)
+            for(size_t i=0; i<n->input_edges.size(); ++i)
             {
-                if(!n.input_edges[i].other) continue;
-                g.draw_circle(n.input_edges[i].other->get_output_location(n.input_edges[i].pin), 7, {1,1,1,1});
-                g.draw_circle(n.get_input_location(i), 7, {1,1,1,1});
+                if(!n->input_edges[i].other) continue;
+                g.draw_circle(get_output_location(*n->input_edges[i].other, n->input_edges[i].pin), 7, {1,1,1,1});
+                g.draw_circle(get_input_location(*n, i), 7, {1,1,1,1});
             }
         }
 
         // If the user is currently dragging a wire between two pins, draw it
-        if(g.is_pressed(1))
+        if(g.is_pressed(ID_NEW_WIRE))
         {
             auto p0 = g.get_cursor(), p3 = p0;
             if(link_output_node)
             {
-                auto loc = link_output_node->get_output_location(link_output_pin);
+                auto loc = get_output_location(*link_output_node, link_output_pin);
                 g.draw_circle(loc, 7, {1,1,1,1});
                 p0 = float2(loc);
             }
             if(link_input_node)
             {
-                auto loc = link_input_node->get_input_location(link_input_pin);
+                auto loc = get_input_location(*link_input_node, link_input_pin);
                 g.draw_circle(loc, 7, {1,1,1,1});
                 p3 = float2(loc);
             }
             const auto p1 = float2((p0.x+p3.x)/2, p0.y), p2 = float2((p0.x+p3.x)/2, p3.y);
             g.draw_bezier_curve(p0, p1, p2, p3, 2, {1,1,1,1});
 
-            if(g.check_release(1)) reset_link();
+            if(g.check_release(ID_NEW_WIRE)) reset_link();
+        }
+
+        if(g.is_mouse_down(GLFW_MOUSE_BUTTON_RIGHT))
+        {
+            g.set_pressed(ID_POPUP_MENU);
+            g.click_offset = g.in.cursor;
+        }
+
+        if(g.is_pressed(ID_POPUP_MENU))
+        {
+            int w = 0, h = -4;
+            for(auto & type : types)
+            {
+                w = std::max(w, g.sprites.default_font.get_text_width(type.caption));
+                h += g.sprites.default_font.line_height + 4;
+            }            
+
+            auto loc = int2(g.click_offset);
+
+            g.begin_overlay();
+            g.draw_rect({loc.x, loc.y, loc.x + w, loc.y + h}, {0.5f,0.5f,0.5f,1});
+            for(auto & type : types)
+            {
+                rect r = {loc.x, loc.y, loc.x + 100, loc.y + g.sprites.default_font.line_height};
+                if(g.check_click(ID_POPUP_MENU, r))
+                {
+                    nodes.push_back(new node{&type, {loc.x, loc.y, loc.x+100, loc.y+100}});
+                    g.pressed_id = {};
+                }
+                g.draw_shadowed_text(loc, type.caption, {1,1,1,1});
+                loc.y += g.sprites.default_font.line_height + 4;
+            }
+            g.end_overlay();
         }
 
         // Do the scrollable, zoomable background
-        scrollable_zoomable_background(g, id, view);
+        scrollable_zoomable_background(g, ID_DRAG_GRAPH, view);
 
         g.end_transform();
 
@@ -232,14 +279,13 @@ int main()
 
     GLuint tex = make_sprite_texture_opengl(g.sprites.sheet);
 
-    const node_type type = {"Graph Node with a long title that will clip the edge of the node", {"Input 1", "Input 2"}, {"Output 1", "Output 2", "Output 3"}};
     graph gr;
     gr.view = {1,{0,0}};
     gr.nodes = {
-        {&type, {50,50,300,250}},
-        {&type, {650,150,900,350}}
+        new node{&types[0], {50,50,300,250}},
+        new node{&types[1], {650,150,900,350}}
     };
-    gr.nodes[1].input_edges[1] = edge(&gr.nodes[0], 1);
+    gr.nodes[1]->input_edges[1] = edge(gr.nodes[0], 0);
 
     while(!glfwWindowShouldClose(win))
     {
